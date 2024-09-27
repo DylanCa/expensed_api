bc4d2dd661296a850c263df16d57ddb779e66e94
DylanCa <dylan.cattelan@gmail.com>
2024-09-27 19:04:36 +0200

    Renamed project to expensed_api

--- expensed_api/asgi.py
+++ expensed_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for expensed project.
+ASGI config for expensed_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expensed.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expensed_api.settings")
 
 application = get_asgi_application()
--- expensed_api/wsgi.py
+++ expensed_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for expensed project.
+WSGI config for expensed_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expensed.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expensed_api.settings")
 
 application = get_wsgi_application()

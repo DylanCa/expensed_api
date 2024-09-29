55556fa2ad9316155b647ca35ef2a68998eefd1d
DylanCa <dylan.cattelan@gmail.com>
2024-09-29 17:07:16 +0200

    Added Colour and Icon to Category model

--- api/models/category.py
+++ api/models/category.py
@@ -7,5 +7,7 @@ from api.models import User
 class Category(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
+    colour_hex = models.CharField(max_length=9)
+    icon = models.CharField(max_length=100)
     household = models.ForeignKey('Household', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)

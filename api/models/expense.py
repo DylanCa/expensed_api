f3c094a2f453e0e4ad9854afc9d4c19fe49017e4
DylanCa <dylan.cattelan@gmail.com>
2024-09-29 16:29:46 +0200

    Updated Expense fields to have proper field types and accurate data

--- api/models/expense.py
+++ api/models/expense.py
@@ -1,13 +1,19 @@
 import uuid
 from django.db import models
-from django.conf import settings
+
+from api.models import User
+
 
 class Expense(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255)
-    date = models.DateField()
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     household = models.ForeignKey('Household', on_delete=models.CASCADE)
+    merchant = models.ForeignKey('Merchant', on_delete=models.CASCADE)
+    payment_type = models.ForeignKey('PaymentType', on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
 

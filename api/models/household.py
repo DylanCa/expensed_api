60c2a69720a6183a1edc38ef55ffb6b55fccd8ac
DylanCa <dylan.cattelan@gmail.com>
2024-09-29 16:28:53 +0200

    Added User(Owner) to related models

--- api/models/household.py
+++ api/models/household.py
@@ -2,7 +2,11 @@ import uuid
 from django.db import models
 from django.conf import settings
 
+from api.models import User
+
+
 class Household(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='households')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- api/models/merchant.py
+++ api/models/merchant.py
@@ -1,7 +1,11 @@
 import uuid
 from django.db import models
 
+from api.models import User
+
+
 class Merchant(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     household = models.ForeignKey('Household', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
